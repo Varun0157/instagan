bf77a31dba0c18e64e2de11569bbee1ab36747b4
Varun0157 <varun.edachali@gmail.com>
2025-02-23 16:59:32 +0530

    test: make segonlymodel returns zeros

--- train.py
+++ train.py
@@ -99,7 +99,6 @@ if __name__ == "__main__":
     opt.model = "seg_only"
     seg_only_model = train(opt)
     assert type(seg_only_model) is SegOnlyModel
-    seg_only_model.eval()
 
     opt.name = name
     opt.model = "insta_gan"
